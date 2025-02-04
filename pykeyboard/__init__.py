9bbe778076a32348b3f629fd818c8c6186f9c6e6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-04 13:39:17 +0530

    Update __init__.py

--- pykeyboard/__init__.py
+++ pykeyboard/__init__.py
@@ -1,9 +1,5 @@
-__title__ = "pykeyboard"
-__version__ = "0.1.5"
-__author__ = "PyMaster"
-__license__ = "MIT License"
-__copyright__ = "Copyright 2020-2022 PyMaster"
-
 from .inline_keyboard import InlineKeyboard
 from .inline_pagination_keyboard import InlinePaginationKeyboard
 from .reply_keyboard import ForceReply
+
+__version__ = "0.1.5"
