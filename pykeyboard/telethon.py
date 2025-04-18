c2b093d725222ecfac65971a10bb20a98586ea38
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-18 14:13:56 +0530

    Update telethon.py

--- pykeyboard/telethon.py
+++ pykeyboard/telethon.py
@@ -4,26 +4,33 @@ except ImportError:
     ReplyInlineMarkup = KeyboardButtonRow = None
 
 
-class InlineKeyboard(ReplyInlineMarkup):
-    def __init__(self, row_width=3):
-        self.row_width = row_width
-        self.rows = []
-        if KeyboardButtonRow and ReplyInlineMarkup is None:
+if ReplyInlineMarkup is not None:
+    class InlineKeyboard(ReplyInlineMarkup):
+        def __init__(self, row_width=3):
+            if KeyboardButtonRow is None:
+                raise ImportError(
+                    "Telethon module is not installed. Please install it using 'pip install telethon'"
+                )
+            self.row_width = row_width
+            self.rows = []
+
+        def add(self, *args):
+            self._check_buttons(args)
+            for i in range(0, len(args), self.row_width):
+                row = args[i : i + self.row_width]
+                self.rows.append(KeyboardButtonRow(buttons=list(row)))
+
+        def row(self, *args):
+            self._check_buttons(args)
+            self.rows.append(KeyboardButtonRow(buttons=list(args)))
+
+        def _check_buttons(self, buttons):
+            for btn in buttons:
+                if getattr(btn, "SUBCLASS_OF_ID", None) != 0xBAD74A3:
+                    raise TypeError("TODO")
+else:
+    class InlineKeyboard:
+        def __init__(self, *args, **kwargs):
             raise ImportError(
                 "Telethon module is not installed. Please install it using 'pip install telethon'"
             )
-
-    def add(self, *args):
-        self._check_buttons(args)
-        for i in range(0, len(args), self.row_width):
-            row = args[i : i + self.row_width]
-            self.rows.append(KeyboardButtonRow(buttons=list(row)))
-
-    def row(self, *args):
-        self._check_buttons(args)
-        self.rows.append(KeyboardButtonRow(buttons=list(args)))
-
-    def _check_buttons(self, buttons):
-        for btn in buttons:
-            if getattr(btn, "SUBCLASS_OF_ID", None) != 0xBAD74A3:
-                raise TypeError("TODO")
